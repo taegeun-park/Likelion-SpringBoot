de6ef51f902969828b0e573e7ec7516f07a5345c
DESKTOP-RJC8TSU\taege <taegeun0525@gmail.com>
2022-10-27 15:05:06 +0900

    Put 컨트롤러 수정

--- src/main/java/com/springboot/hello/controller/PostController.java
+++ src/main/java/com/springboot/hello/controller/PostController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/v1/post-api")
+@RequestMapping("/api/v1/put-api")
 public class PostController {
 //
 //    @RequestMapping(value="/domain", method= RequestMethod.POST)
