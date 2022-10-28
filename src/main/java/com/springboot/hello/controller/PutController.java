ffaf62a63d8410dee9cabf76575ef7318d045412
DESKTOP-RJC8TSU\taege <taegeun0525@gmail.com>
2022-10-28 11:38:54 +0900

    log 남기기

--- src/main/java/com/springboot/hello/controller/PutController.java
+++ src/main/java/com/springboot/hello/controller/PutController.java
@@ -1,6 +1,7 @@
 package com.springboot.hello.controller;
 
 import com.springboot.hello.domain.dto.MemberDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,8 +10,8 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("api/v1/put-api")
+@Slf4j
 public class PutController {
-
     @RequestMapping(value = "/domain", method = RequestMethod.POST)
     public String postExample() {
         return "Hello Post API";
@@ -18,6 +19,7 @@ public class PutController {
 
     @PutMapping("/member3")
     public ResponseEntity<MemberDto> putMember(@RequestBody MemberDto memberDto) {
+        log.info("member3로 요청이 들어왔습니다.");
         return ResponseEntity
                 .status(HttpStatus.ACCEPTED)
                 .body(memberDto);
