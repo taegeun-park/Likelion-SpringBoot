16c44a8d78ecb6f8d44a1f4f46b5694b5a577a0e
DESKTOP-RJC8TSU\taege <taegeun0525@gmail.com>
2022-10-28 11:18:26 +0900

    Swagger적용

--- src/main/java/com/springboot/hello/controller/PutController.java
+++ src/main/java/com/springboot/hello/controller/PutController.java
@@ -16,19 +16,7 @@ public class PutController {
         return "Hello Post API";
     }
 
-    @PostMapping("/member")
-    public String postMember(@RequestBody Map<String, Object> postData) {
-        StringBuilder sb = new StringBuilder();
-        postData.entrySet().forEach(map->sb.append(map.getKey() + ":" + map.getValue() + "\n"));
-        return sb.toString();
-    }
-
-    @PostMapping("/member2")
-    public String postMember(@RequestBody MemberDto memberDto) {
-        return memberDto.toString();
-    }
-
-    @PostMapping("/member3")
+    @PutMapping("/member3")
     public ResponseEntity<MemberDto> putMember(@RequestBody MemberDto memberDto) {
         return ResponseEntity
                 .status(HttpStatus.ACCEPTED)
