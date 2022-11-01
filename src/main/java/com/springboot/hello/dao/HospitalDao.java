2232c2480a6f334cf169746096274b4f7d883df9
DESKTOP-RJC8TSU\taege <taegeun0525@gmail.com>
2022-11-02 01:38:28 +0900

    [add] rowMapper 매핑 전체

--- src/main/java/com/springboot/hello/dao/HospitalDao.java
+++ src/main/java/com/springboot/hello/dao/HospitalDao.java
@@ -14,8 +14,18 @@ public class HospitalDao {
         Hospital hospital = new Hospital();
         hospital.setId(rs.getInt("id"));
         hospital.setOpenServiceName(rs.getString("open_service_name"));
-        hospital.setHospitalName(rs.getString("hospital_name"));
+        hospital.setOpenLocalGovernmentCode(rs.getInt("open_local_government_code"));
+        hospital.setManagementNumber(rs.getString("management_number"));
         hospital.setLicenseData(rs.getTimestamp("license_date").toLocalDateTime());
+        hospital.setBusinessStatus(rs.getInt("business_status"));
+        hospital.setBusinessStatusCode(rs.getInt("business_status_code"));
+        hospital.setPhone(rs.getString("phone"));
+        hospital.setFullAddress(rs.getString("full_address"));
+        hospital.setRoadNameAddress(rs.getString("road_name_address"));
+        hospital.setHospitalName(rs.getString("hospital_name"));
+        hospital.setHealthcareProviderCount(rs.getInt("healthcare_provider_count"));
+        hospital.setPatientRoomCount(rs.getInt("patient_room_count"));
+        hospital.setTotalNumberOfBeds(rs.getInt("total_number_of_beds"));
         hospital.setTotalAreaSize(rs.getFloat("total_area_size"));
         return hospital;
     };
@@ -26,19 +36,6 @@ public class HospitalDao {
 
     //List<Hospital> 11만건
     public void addHospital(Hospital hospital) {
-//        this.jdbcTemplate.update("INSERT INTO nation_wide_hospitals(`id`, `open_service_name`, " +
-//                        "`open_local_government_code`, `management_number`, `license_date`, `business_status`, " +
-//                        "`business_status_code`, `phone`, `full_address`, `road_name_address`, `hospital_name`, " +
-//                        "`business_type_name`, `healthcare_provider_count`, `patient_room_count`, `total_number_of_beds`, " +
-//                        "`total_area_size`) " +
-//                        "VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
-//                hospital.getId(), hospital.getOpenServiceName(), hospital.getOpenLocalGovernmentCode(),
-//                hospital.getManagementNumber(), hospital.getLicenseData(), hospital.getBusinessStatus(),
-//                hospital.getBusinessStatusCode(), hospital.getPhone(),hospital.getFullAddress(),
-//                hospital.getRoadNameAddress(), hospital.getHospitalName(), hospital.getBusinessTypeName(),
-//                hospital.getHealthcareProviderCount(), hospital.getPatientRoomCount(), hospital.getTotalNumberOfBeds(),
-//                hospital.getTotalAreaSize());
-
         String sql = "INSERT INTO `likelion-db`.`nation_wide_hospitals` (`id`, `open_service_name`, `open_local_government_code`, `management_number`, `license_date`, `business_status`, `business_status_code`, `phone`, `full_address`, `road_name_address`, `hospital_name`, `business_type_name`, `healthcare_provider_count`, `patient_room_count`, `total_number_of_beds`, `total_area_size`)" +
                 " VALUES (?,?,?," +
                 "?,?,?," +
@@ -53,6 +50,20 @@ public class HospitalDao {
                 hospital.getHealthcareProviderCount(), hospital.getPatientRoomCount(), hospital.getTotalNumberOfBeds(),
                 hospital.getTotalAreaSize()
         );
+
+//        this.jdbcTemplate.update("INSERT INTO nation_wide_hospitals(`id`, `open_service_name`, " +
+//                        "`open_local_government_code`, `management_number`, `license_date`, `business_status`, " +
+//                        "`business_status_code`, `phone`, `full_address`, `road_name_address`, `hospital_name`, " +
+//                        "`business_type_name`, `healthcare_provider_count`, `patient_room_count`, `total_number_of_beds`, " +
+//                        "`total_area_size`) " +
+//                        "VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
+//                hospital.getId(), hospital.getOpenServiceName(), hospital.getOpenLocalGovernmentCode(),
+//                hospital.getManagementNumber(), hospital.getLicenseData(), hospital.getBusinessStatus(),
+//                hospital.getBusinessStatusCode(), hospital.getPhone(),hospital.getFullAddress(),
+//                hospital.getRoadNameAddress(), hospital.getHospitalName(), hospital.getBusinessTypeName(),
+//                hospital.getHealthcareProviderCount(), hospital.getPatientRoomCount(), hospital.getTotalNumberOfBeds(),
+//                hospital.getTotalAreaSize());
+
 
     }
 
